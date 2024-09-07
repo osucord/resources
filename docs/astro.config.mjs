6430aebfb2e069c33e623257fff8d1ed031c4300
Ernestas Kluonis <ekgame1@gmail.com>
2024-09-07 22:11:43 +0300

    Added test configuration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://ekgame.github.io',
+  base: 'osucord-resources',
 });
